bd4c7176fd673d82f7cf4681bc5133d10db59e78
a6haji <aly.haji99@gmail.com>
2019-06-04 13:05:38 -0700

    Created Landmarks SwiftUI Project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Aly Haji on 2019-06-04.
+//  Copyright © 2019 Aly Haji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        VStack (alignment: .leading){
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+            
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
